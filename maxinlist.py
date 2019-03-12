a1be96d10c79514a8412ecc7ee9a8f6d623a24a6
ramazansahin <ramazannsahin52@gmail.com>
2019-03-12 17:28:09 +0300

    maxinlist düzeltildi

--- maxinlist.py
+++ maxinlist.py
@@ -1,7 +1,7 @@
 mylist=[4,-3,2,5,-4,1,4,6,-7]
-mylist2=[2,-5,4,-7,5,3,4,12]
-sums=[]
+mylist2=[2,-5,4,-7,5,0,-4,12]
 def maxinlist(alist):
+    sums=[]
     for i in range(0,len(alist)):
         for j in range(len(alist),0,-1):
             sums.append(sum(alist[i:j]))
